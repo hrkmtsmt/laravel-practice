b7205169632066078f4750b17a46492dfb4328a8
hrkmtsmt <hrkmtsmt@icloud.com>
2022-10-07 16:29:23 +0900

    :tada: [INITIAL COMMIT]: First commit

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+    public function test_body()
+    {
+        $response = $this->get('/');
+        $response->assertSeeText('Hello Laravel');
+    }
+}
